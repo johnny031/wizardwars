14af9c2824186476a788d8b18931fc93890b502c
Johnny <johnny055238@gmail.com>
2020-02-23 21:58:25 +0800

    test scale.mode:Phaser.Scale.REZIZE effect

--- server/public/js/game.js
+++ server/public/js/game.js
@@ -1,10 +1,10 @@
 var config = {
   backgroundColor: 0x000000,
   scale: {
-    mode: Phaser.Scale.FIT,
-    autoCenter: Phaser.Scale.CENTER_BOTH,
     width: 400,
-    height: 544
+    height: 544,
+    mode: Phaser.Scale.RESIZE,
+    autoCenter: Phaser.Scale.CENTER_BOTH
   },
   scene: [Scene1, Scene2],
   pixelArt: true,
