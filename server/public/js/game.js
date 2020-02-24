06c3848193e8993cc38507b455a2f76ef5d86725
Johnny <johnny055238@gmail.com>
2020-02-24 14:52:57 +0800

    test

--- server/public/js/game.js
+++ server/public/js/game.js
@@ -13,10 +13,8 @@ window.mobileAndTabletcheck = function() {
   })(navigator.userAgent || navigator.vendor || window.opera);
   return check;
 };
-var width = window.mobileAndTabletcheck() ? 400 : 400;
-var height = window.mobileAndTabletcheck()
-  ? (400 * window.innerHeight) / window.innerWidth
-  : 544;
+var width = 400;
+var height = (400 * window.innerHeight) / window.innerWidth;
 var config = {
   backgroundColor: 0x000000,
   scale: {
