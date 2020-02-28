01635c9bdb7a8cee764c6f745b30adc11aad3d6c
Johnny <johnny055238@gmail.com>
2020-02-28 14:26:46 +0800

    set loadBar to refresh 5 times/sec

--- server/public/js/Scene2.js
+++ server/public/js/Scene2.js
@@ -629,54 +629,54 @@ class Scene2 extends Phaser.Scene {
       if (selected_bullet === 1) {
         var loadBar = loadBar1_1;
         var t = t1_1;
-        var coolDownTime = 360 / (5 * 10);
+        var coolDownTime = 360 / (5 * 5);
       } else if (selected_bullet === 2) {
         var loadBar = loadBar2_1;
         var t = t2_1;
-        var coolDownTime = 360 / (6 * 10);
+        var coolDownTime = 360 / (6 * 5);
       } else if (selected_bullet === 3) {
         var loadBar = loadBar3_1;
         var t = t3_1;
-        var coolDownTime = 360 / (8 * 10);
+        var coolDownTime = 360 / (8 * 5);
       } else if (selected_bullet === 4) {
         var loadBar = loadBar4_1;
         var t = t4_1;
-        var coolDownTime = 360 / (9 * 10);
+        var coolDownTime = 360 / (9 * 5);
       } else if (selected_bullet === 5) {
         var loadBar = loadBar5_1;
         var t = t5_1;
-        var coolDownTime = 360 / (7 * 10);
+        var coolDownTime = 360 / (7 * 5);
       } else if (selected_bullet === 6) {
         var loadBar = loadBar6_1;
         var t = t6_1;
-        var coolDownTime = 360 / (10 * 10);
+        var coolDownTime = 360 / (10 * 5);
       }
     } else {
       var start_point = Math.PI / 2;
       if (selected_bullet === 1) {
         var loadBar = loadBar1_2;
         var t = t1_2;
-        var coolDownTime = 360 / (5 * 10);
+        var coolDownTime = 360 / (5 * 5);
       } else if (selected_bullet === 2) {
         var loadBar = loadBar2_2;
         var t = t2_2;
-        var coolDownTime = 360 / (6 * 10);
+        var coolDownTime = 360 / (6 * 5);
       } else if (selected_bullet === 3) {
         var loadBar = loadBar3_2;
         var t = t3_2;
-        var coolDownTime = 360 / (8 * 10);
+        var coolDownTime = 360 / (8 * 5);
       } else if (selected_bullet === 4) {
         var loadBar = loadBar4_2;
         var t = t4_2;
-        var coolDownTime = 360 / (9 * 10);
+        var coolDownTime = 360 / (9 * 5);
       } else if (selected_bullet === 5) {
         var loadBar = loadBar5_2;
         var t = t5_2;
-        var coolDownTime = 360 / (7 * 10);
+        var coolDownTime = 360 / (7 * 5);
       } else if (selected_bullet === 6) {
         var loadBar = loadBar6_2;
         var t = t6_2;
-        var coolDownTime = 360 / (10 * 10);
+        var coolDownTime = 360 / (10 * 5);
       }
     }
     var loadBar_interval = setInterval(() => {
@@ -692,7 +692,7 @@ class Scene2 extends Phaser.Scene {
         clearInterval(loadBar_interval);
         t = 0.0;
       }
-    }, 100);
+    }, 200);
   }
   shootOrMove1(pointer) {
     if (shoot1) {
